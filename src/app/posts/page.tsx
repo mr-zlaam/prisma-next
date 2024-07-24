ef4f39d632f4e6a057693f732c9c13f8b31d2a3f
Zlaam <mrzalaam@gmail.com>
2024-07-24 12:47:30 +0500

    update 4 files and create 1 file

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,16 @@
+import {} from "react";
+
+function Post() {
+  const postCount = 0;
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl font-bold text-center my-10">
+          ALL POSTS:({postCount}){" "}
+        </h1>
+      </section>
+    </>
+  );
+}
+
+export default Post;
